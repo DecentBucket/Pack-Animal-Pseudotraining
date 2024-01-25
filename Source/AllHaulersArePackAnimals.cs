9ffb90db5546422632ce896409a6b412a11c4a7a
DecentBucket <141464482+DecentBucket@users.noreply.github.com>
2024-01-24 18:03:11 -0800

    Functioning settings and selectable trainables!
    Formatting needs some work though.

--- Source/AllHaulersArePackAnimals.cs
+++ Source/AllHaulersArePackAnimals.cs
@@ -14,8 +14,8 @@ namespace DB_All_Haulers_Are_Pack_Animals
      * 
      * Notes:
      * If a trained animal loses the skill due to decay while forming a caravan, formation procceds regardless, giving items to other pawns capable of carrying items, even exceeding their carrying capacity, however the caravan will become immobile as soon as it departs. 
-     * Not a problem if you are sending a caravan from the colony, but if this happens while forming a new caravan from a temporary location, the player will have to abandon some items
-     * Strangely, this appears to be vanilla behavior, as the same thing will happen if a pack animal dies during formation.
+     * Not a problem if you are sending a caravan from the colony, but if this happens while forming a new caravan from a temporary location, the player will have to abandon some items.
+     * This appears to be vanilla behavior, as the same thing will happen if a pack animal dies during formation.
      * 
      * As of the current version (1.4.3901) animals will not lose training steps or skills while in a caravan on the world map, this is likely intended as the vanilla game has no way of training animals in a caravan
      * The Vanilla game picks pack animals for their trade caravans based on a list that is defined by the faction def and biome def, so traders by default ignore animal training altoghether
@@ -26,11 +26,12 @@ namespace DB_All_Haulers_Are_Pack_Animals
     public class Db_AllHaulersArePackAnimals : Mod
     {
 
-        Db_Haulers_Settings settings;
+        public static Db_Haulers_Settings settings;
 
         public Db_AllHaulersArePackAnimals(ModContentPack content) : base(content)
         {
-            this.settings = GetSettings<Db_Haulers_Settings>();
+            settings = GetSettings<Db_Haulers_Settings>();
+            new Harmony("DecentBucket.AllHaulersArePackAnimals.patch").PatchAll();
         }
 
         public override string SettingsCategory()
@@ -43,23 +44,77 @@ namespace DB_All_Haulers_Are_Pack_Animals
             Listing_Standard listing_Standard = new Listing_Standard();
             listing_Standard.Begin(inRect);
             listing_Standard.CheckboxLabeled("RequirePackAnimalPropName".Translate(), ref settings.requirePackAnimalProp, "RequirePackAnimalPropToolTip".Translate());
+            listing_Standard.ColumnWidth += 35f;
+            Text.Font = GameFont.Medium;
+            listing_Standard.Label("RequiredTrainingsSectionTitle".Translate());
+            Text.Font = GameFont.Small;
+            listing_Standard.ColumnWidth -= 35f;
+            listing_Standard.GapLine();
+            listing_Standard.Gap();
+            for (int i = 0; i < settings.LoadedTrainables.Count; i++)
+            {
+                TrainableDef key = TrainableUtility.TrainableDefsInListOrder[i];
+                //TrainableDef key = settings.loadedTrainables.ElementAt(i).Key;
+                bool value = settings.LoadedTrainables[key];
+                listing_Standard.CheckboxLabeled((string)key.LabelCap, ref value, "RequiredTrainingsSectionToolTip".Translate(key.LabelCap));
+                settings.trainableNames[key.defName] = value;
+                settings.LoadedTrainables[key] = value;
+                
+            }
             listing_Standard.End();
             base.DoSettingsWindowContents(inRect);
         }
 
         //----START OF HARMONY PATCH CODE----
 
-
-        static Db_AllHaulersArePackAnimals()
-        {
-            new Harmony("DecentBucket.AllHaulersArePackAnimals.patch").PatchAll();
-        }
-
         static bool ShouldCountAsPackAnimal(Pawn p)
         {
-            //Get the mod setting
+            //Get the mod settings
             bool packAnimalPropRequired = LoadedModManager.GetMod<Db_AllHaulersArePackAnimals>().GetSettings<Db_Haulers_Settings>().requirePackAnimalProp;
+            IEnumerable<TrainableDef> requiredTrainables = LoadedModManager.GetMod<Db_AllHaulersArePackAnimals>().GetSettings<Db_Haulers_Settings>().requiredTrainables;
 
+            if (p.training != null)
+            {
+                //can the pawn even be trained with the required trainables?
+                foreach (TrainableDef td in requiredTrainables)
+                {
+                    if (!p.training.CanAssignToTrain(td))
+                    {
+                        return p.RaceProps.packAnimal;
+                    }
+                }
+                //at this point in the code, the pawn actually matters to the mod, do the thing!
+
+                //does the pawn have ALL the required training?
+                foreach (TrainableDef td in requiredTrainables)
+                {
+                    if (!p.training.HasLearned(td))
+                    {
+                        //pawn is missing one (or more) required trainables
+                        return false;
+                    }
+                }
+                //pawn has training, can learn, and has learned all required trainables, just one last thing to check
+                return packAnimalPropRequired ? p.RaceProps.packAnimal : true;
+            }
+            else
+            {
+                return p.RaceProps.packAnimal;
+            }
+
+            /*
+            for (int i = 0; i < loadedTrainables.Count(); i++)
+            {
+                if (loadedTrainables.ElementAt(i).Value)
+                { 
+                    if (p.training != null)
+                    {
+
+                    }
+                }
+            }
+
+            /*
             //check if the pawn can be trained for hauling
             if(p.training != null && p.training.CanAssignToTrain(DefDatabase<TrainableDef>.GetNamed("Haul")))
             {
@@ -73,9 +128,11 @@ namespace DB_All_Haulers_Are_Pack_Animals
             {
                 return p.RaceProps.packAnimal;
             }
+            */
         }
 
-        /*static void CheckCaravanAndMassCapacity(TrainableDef trainableDef, Pawn p)
+        /* Leftover from my attempt at trying to fix the changing capacity problem from the game (read comment at the top of the file) might reuse as a seperate mod
+        static void CheckCaravanAndMassCapacity(TrainableDef trainableDef, Pawn p)
         {
             //Log.Message(trainableDef.ToString() + " " + p.ToString() + " " + p.IsFormingCaravan().ToStringYesNo());
             //was the decayed skill haul, and was the skill forgotten?
@@ -118,7 +175,8 @@ namespace DB_All_Haulers_Are_Pack_Animals
         }
 
         //Patch for the case where an animal loses haul training while forming a caravan
-        /*[HarmonyPatch(typeof(Pawn_TrainingTracker), nameof(Pawn_TrainingTracker.TrainingTrackerTickRare))]
+        /* Also leftover from my attempts to fix the changing capacity problem from the game (again, read the comment at the top of the file)
+        [HarmonyPatch(typeof(Pawn_TrainingTracker), nameof(Pawn_TrainingTracker.TrainingTrackerTickRare))]
         [HarmonyTranspiler]
         [HarmonyDebug]
         static IEnumerable<CodeInstruction> TrainspilerTrainingTrackerTickRare(IEnumerable<CodeInstruction> instructions, ILGenerator il)
--- Source/Db_Haulers_Settings.cs
+++ Source/Db_Haulers_Settings.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DB_All_Haulers_Are_Pack_Animals;
+using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,13 +12,80 @@ namespace All_Haulers_Are_Pack_Animals
     /*
      * Standard mod settings stuff here 
      */
-    internal class Db_Haulers_Settings : ModSettings
+    public class Db_Haulers_Settings : ModSettings
     {
         public bool requirePackAnimalProp;
+
+        public Dictionary<string, bool> trainableNames = new Dictionary<string, bool>();
+        public List<string> trainables = new List<string>();
+        public List<bool> valid = new List<bool>();
+        public Dictionary<TrainableDef, bool> cachedTrainables = new Dictionary<TrainableDef, bool>();
+        public Dictionary<TrainableDef, bool> LoadedTrainables
+        {
+            get
+            {
+                if(cachedTrainables.NullOrEmpty())
+                {
+                    for (int i = 0; i < trainableNames.Count; i++)
+                    {
+                        
+                        TrainableDef namedSilentFail = DefDatabase<TrainableDef>.GetNamedSilentFail(trainableNames.ElementAt(i).Key);
+                        if (namedSilentFail != null )
+                        {
+                            cachedTrainables.Add(namedSilentFail, trainableNames.ElementAt(i).Value);
+                        }
+                    }
+                }
+                return cachedTrainables;
+            }
+        }
+
+        //IEnumerable with only the trainables selected in the mod settings
+        public IEnumerable<TrainableDef> requiredTrainables => from td in LoadedTrainables where LoadedTrainables[td.Key] select td.Key;
 
         public override void ExposeData()
         {
             Scribe_Values.Look(ref requirePackAnimalProp, "requirePackAnimalProp", true);
+            Scribe_Collections.Look(ref trainableNames, "trainableNames");
         }
+    }
+
+    [StaticConstructorOnStartup]
+    public static class Db_Haulers_Startup
+    {
+        public static void CheckTrainables()
+        {
+            //Log.Message((Db_AllHaulersArePackAnimals.settings != null).ToString());
+            Db_Haulers_Settings settings = Db_AllHaulersArePackAnimals.settings;
+            if (settings.trainableNames.NullOrEmpty())
+            {
+                settings.trainableNames = new Dictionary<string, bool>();
+            }
+            foreach (TrainableDef item in DefDatabase<TrainableDef>.AllDefs.ToList())
+            {
+                Log.Message(item.ToString() + " " + (settings.trainableNames != null).ToString());
+                if (item != null && !settings.trainableNames.ContainsKey(item.defName))
+                {
+                    if (item.defName == "Haul")
+                    {
+                        settings.trainableNames.Add(item.defName, true);
+                        settings.trainables.Add(item.defName);
+                        settings.valid.Add(true);
+                    }
+                    else
+                    {
+                        settings.trainableNames.Add(item.defName, false);
+                        settings.trainables.Add(item.defName);
+                        settings.valid.Add(false);
+                    }
+                }
+            }
+        }
+        
+        static Db_Haulers_Startup()
+        {
+            CheckTrainables();
+        }
+        
     }
 }
